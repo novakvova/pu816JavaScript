f8445196065e5d2c39334d65e348037811b35550
novakvova <novakvova@gmail.com>
2020-06-08 12:04:00 +0300

    input

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ window.onload = function () {
     clicker.onclick=function () {
         info.innerHTML += '<a href="#">'+fruits[i]+'</a><br/>';
         i++;
+        var text_one = document.getElementById("txt_one");
+        console.log("Input value", text_one.value);
         //alert("click span");
     }
 }
